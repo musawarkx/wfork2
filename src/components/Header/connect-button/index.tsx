ec4c4d607f7148daa423fd278d89309091d5bee8
musawarkx <36422741+musawarkx@users.noreply.github.com>
2021-12-06 17:23:15 -0500

    Update index.tsx

--- src/components/Header/connect-button/index.tsx
+++ src/components/Header/connect-button/index.tsx
@@ -30,7 +30,7 @@ function ConnectMenu() {
         clickFunc = () => {};
     }
 
-    if (isConnected && providerChainID !== DEFAULD_NETWORK) {
+    if (providerChainID !== DEFAULD_NETWORK) {
         buttonText = "Wrong network";
         buttonStyle = { backgroundColor: "rgb(255, 67, 67)" };
         clickFunc = () => {
